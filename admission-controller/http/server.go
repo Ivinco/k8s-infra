8a6ea7a858e41b02509a679a6189669459e984f5
bg502 <andrew.kozlowskiy@gmail.com>
2023-04-21 16:55:45 +0400

    tuned admission controller deployment

--- admission-controller/http/server.go
+++ admission-controller/http/server.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"net/http"
 
-	"admissioncontroller/probesValidation"
+	"admissioncontroller/validation"
 )
 
 // NewServer creates and return a http.Server
 func NewServer(port string) *http.Server {
 	// Instances hooks
-	probesValidation := probesValidation.NewValidationHook()
+	validation := validation.NewValidationHook()
 
 	// Routers
 	ah := newAdmissionHandler()
 	mux := http.NewServeMux()
 	mux.Handle("/healthz", healthz())
-	mux.Handle("/validate/probes", ah.Serve(probesValidation))
+	mux.Handle("/validate", ah.Serve(validation))
 
 	return &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
