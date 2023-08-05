e184a3b83d6593e6a23d1cac28f32242e8d38fbe
bg502 <andrew.kozlowskiy@gmail.com>
2023-08-05 18:42:50 +0300

    force rebuilding of stages

--- werfConverge.sh
+++ werfConverge.sh
@@ -2,7 +2,6 @@
 set -x
 export PATH=/root/bin:/usr/local/git/bin:$PATH
 git --version
-sleep 1200
 trdl add werf https://tuf.werf.io 1 b7ff6bcbe598e072a86d595a3621924c8612c7e6dc6a82e919abe89707d7e3f468e616b5635630680dd1e98fc362ae5051728406700e6274c5ed1ad92bea52a2
 source "$(trdl use werf 1.2 stable)"
 werf version
