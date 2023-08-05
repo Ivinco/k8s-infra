0092ebc44ccc1725f647bf2b2f63a487e44d9550
bg502 <andrew.kozlowskiy@gmail.com>
2023-08-05 17:41:13 +0300

    force rebuilding of stages

--- werfConverge.sh
+++ werfConverge.sh
@@ -6,4 +6,4 @@ source "$(trdl use werf 1.2 stable)"
 echo ${WERF_REPO_HARBOR_PASSWORD} |  docker login harbor.sgdctroy.net --username admin --password-stdin
 werf version
 werf render
-werf converge
+werf build
