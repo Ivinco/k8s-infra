d02c39ad7d9726625d13669f0f7c5aee39aa1d75
bg502 <andrew.kozlowskiy@gmail.com>
2023-08-05 21:41:47 +0300

    force rebuilding of stages

--- werfConverge.sh
+++ werfConverge.sh
@@ -8,7 +8,7 @@ pwd
 git --version
 #trdl add werf https://tuf.werf.io 1 b7ff6bcbe598e072a86d595a3621924c8612c7e6dc6a82e919abe89707d7e3f468e616b5635630680dd1e98fc362ae5051728406700e6274c5ed1ad92bea52a2
 #source "$(trdl use werf 1.2 stable)"
-source $("~/bin/trdl" use werf "1.2" "stable")
+source $("/var/go/bin/trdl" use werf "1.2" "stable")
 werf version
 werf cr login -u admin -p ${WERF_REPO_HARBOR_PASSWORD} harbor.sgdctroy.net
 werf render
